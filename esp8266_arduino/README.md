6bdc9794e24aef4b5ee353be20a65a40c8de6257
Vulcan-YJX <impulse959>
2020-02-23 11:15:36 +0800

    如易物联

---
+++ esp8266_arduino/README.md
@@ -0,0 +1,10 @@
+RYKJ
+===
+ ## Windows/Linux
+
+> ### [ Python3文本传输例程](https://github.com/Vulcan-YJX/RYKJ/blob/master/python3/reconnect.py)
+
+> ### [Python3图像传输例程](https://github.com/Vulcan-YJX/RYKJ/blob/master/python3/picServer.py)
+
+ ## ESP8266
+> ### [eps8266文本传输例程](https://github.com/Vulcan-YJX/RYKJ/tree/master/esp8266)
---
+++ esp8266_arduino/esp8266_arduino.ino
@@ -0,0 +1,65 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <Ticker.h> 
+
+#define WIFISSID "vulcan-walker"
+#define PASSWD  "vulcan123"
+String Key = "kEyfda520fe5e58b421";
+
+const char *host = "www.rykj.xyz";
+String comdata = "";
+String KeyMark = "<key>"+Key+"</key>";
+
+Ticker flipper;
+WiFiClient client;
+
+void flip() {
+   client.print(KeyMark);
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("Connecting...\n");
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(WIFISSID,PASSWD); // change it to your ussid and password
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    if (!client.connect("www.rykj.xyz", 443))
+    {
+        Serial.println("Connection to host failed");
+        delay(1000);
+        return;
+    }
+    else{
+          client.print(KeyMark);
+      }
+    flipper.attach(180, flip);//每隔0.5秒执行一次回调函数
+}
+
+void loop()
+{
+  while (Serial.available() > 0)  
+    {
+        comdata += char(Serial.read());
+        delay(2);
+    }
+    while (client.available() > 0)
+        {
+          char c = client.read();
+          Serial.write(c);
+        }
+    if (comdata.length() > 0)
+    {
+        String SendMsg = comdata + KeyMark;
+        Serial.println(SendMsg);
+        client.print(SendMsg);
+        comdata = "";
+    }
+
+}
+  
