5e4dcc5f22b6d7fe06b52cb84b102e5930fe8999
Vulcan-YJX <impulse959>
2020-02-27 14:01:59 +0800

    如易物联

---
+++ esp32_arduino/README.md
@@ -0,0 +1,13 @@
+RYKJ
+===
+ ## 开发环境配置
+> ### [ Aruidno配置ESP8266开发环境](https://blog.csdn.net/T_infinity/article/details/104457030)
+
+ ## 快速开始
+ ```修改WIFISSID和PASSWD连接为自己连接wifi的信息``` 
+ 
+ ```修改Key为公众号获取的十九位Key``` 
+
+## 支持硬件
+> ### 此例程支持乐鑫ESP8266大部分常用芯片
+![esp8266](https://mmbiz.qpic.cn/mmbiz_jpg/Zibr0MiccibwpQia6owv6IJ2RicZA617u57vCESr8qgmpqIG48Bibh9hSEWjKgOe7dwHibsJd5dnmbxhnqslxPBibpkgrw/0?wx_fmt=jpeg)
---
+++ esp32_arduino/esp8266_arduino.ino
@@ -0,0 +1,64 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <Ticker.h> 
+
+#define WIFISSID "YOUR WIFI SSID"
+#define PASSWD  "YOUR WIFI PASSWD"
+String Key = "微信获取Key进行替换";
+
+const char *host = "www.rykj.xyz";
+String comdata = "";
+String KeyMark = "<key>"+Key+"</key>";
+
+Ticker flipper;
+WiFiClient client;
+
+void flip() {
+   client.print(KeyMark);
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("Connecting...\n");
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(WIFISSID,PASSWD); // change it to your ussid and password
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    if (!client.connect("www.rykj.xyz", 443))
+    {
+        Serial.println("Connection to host failed");
+        delay(1000);
+        return;
+    }
+    else{
+          client.print(KeyMark);
+      }
+    flipper.attach(180, flip);//每隔0.5秒执行一次回调函数
+}
+
+void loop()
+{
+  while (Serial.available() > 0)  
+    {
+        comdata += char(Serial.read());
+        delay(2);
+    }
+    while (client.available() > 0)
+        {
+          char c = client.read();
+          Serial.write(c);
+        }
+    if (comdata.length() > 0)
+    {
+        String SendMsg = comdata + KeyMark;
+        Serial.println(SendMsg);
+        client.print(SendMsg);
+        comdata = "";
+    }
+
+}
+  
